7c3898670346f4a45f0d8f8d7ecb4992e1b7a45f
Broholm Kristian <kristianb@admdkristianbr.sk.fi.local>
2017-01-26 15:59:17 +0200

    Multiple changes

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*
- * @file:           muPlugin.php
- * @description:    Collection of basic methods for muPlugin to use
+ * @file:           Plugin.php
+ * @description:    Collection of basic methods for Plugin to use
  * @package:        kbwp
  * @author:         kristianb
  * @email:          kristian.broholm@gmail.com
@@ -12,37 +12,8 @@ namespace kbwp;
 
 abstract class Plugin {
     
-    protected $dist;
-    
-    /* Read assets from manifest JSON created by rev-module
-     * @author: kristianb
-     * @since:  1.0.0
-     * @param   $manifest_path  string  Absolute path to manifest.json
-     * @return  $assets         array   Returns assets as key-value pairs
-     * */
-    protected function get_assets_from_manifest( $manifest_path = '' ) {
-        
-        $assets = false;
-        
-        if ( file_exists( $manifest_path ) ) {
-            $manifest   = file_get_contents($manifest_path, true);
-            $assets     = json_decode($manifest);
-        } 
-        
-        return $assets;
+    protected function __construct() {
+        flush_rewrite_rules();
     }
     
-    
-    /* Get asset by reading a key from the manifest file
-     * @author: kristianb
-     * @since:  0.0.0
-     * */
-    protected function get_asset( $asset, $manifest = array() ) {
-        
-        if ( array_key_exists( $asset, $manifest ) ) {
-            return $this->_dist . '/' . $manifest[ $asset ];
-        } 
-
-        return $this->dist . '/' . $asset;
-    }
 }
--- src/kbwp.php
+++ src/kbwp.php
@@ -10,13 +10,43 @@
 
 namespace kbwp;
 
-class kbwp {
-    
-    private function __construct() {}
-    
+abstract class kbwp {
+
     public static function uglify( $string ) {
         $string = strtolower( $string );
         $string = str_replace( array(' ','ä','å','ö'), array( '_','a','a','o' ), $string );
         return $string;
+    }
+    
+    /* Read assets from manifest JSON created by rev-module
+     * @author: kristianb
+     * @since:  1.0.0
+     * @param   $manifest_path  string  Absolute path to manifest.json
+     * @return  $assets         array   Returns assets as key-value pairs
+     * */
+    public static function get_assets_from_manifest( $manifest_path = '' ) {
+        
+        $assets = array();
+        
+        if ( file_exists($manifest_path) ) {
+            $manifest   = file_get_contents($manifest_path, true);
+            $assets     = json_decode($manifest);
+        } 
+        
+        return $assets;
+    }
+    
+    
+    /* Get asset by reading a key from the manifest file
+     * @author: kristianb
+     * @since:  0.0.0
+     * */
+    public static function get_asset($asset, $manifest = array()) {
+        
+        if ( array_key_exists( $asset, $manifest ) ) {
+            return $manifest[ $asset ];
+        } 
+
+        return $asset;
     }
 }
