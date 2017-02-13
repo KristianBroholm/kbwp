ebc3c48f90c18c64182a1546a6ec583d47b2ef34
Broholm Kristian <kristianb@admdkristianbr.sk.fi.local>
2017-02-13 13:52:59 +0200

    Lisätty Teemalle metodi Facebook-pikselin lisäämiseen

--- src/Theme.php
+++ src/Theme.php
@@ -52,4 +52,30 @@ class Theme {
             $editor_styles = array_merge( $editor_styles, $stylesheet );
         }
     }
-}
+    
+    
+    /**
+     * @author  kristianb
+     * @since   1.0.0
+     * @param   $id
+     **/
+    public function add_facebook_pixel( $id ) {
+        
+        add_action( 'wp_head', function( $id ) {
+            echo '<!-- Facebook Pixel Code -->';
+            echo '<script>';
+            echo '!function(f,b,e,v,n,t,s)';
+            echo '{if(f.fbq)return;n=f.fbq=function(){n.callMethod?';
+            echo 'n.callMethod.apply(n,arguments):n.queue.push(arguments)};';
+            echo "if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';";
+            echo "n.queue=[];t=b.createElement(e);t.async=!0;";
+            echo "t.src=v;s=b.getElementsByTagName(e)[0];";
+            echo "s.parentNode.insertBefore(t,s)}(window,document,'script',";
+            echo "'https://connect.facebook.net/en_US/fbevents.js');";
+            echo "fbq('init', '" . $id . "');";
+            echo "fbq('track', 'PageView');";
+            echo "</script>";
+            echo '<noscript><img height="1" width="1" src="https://www.facebook.com/tr?id=' . $id . '&ev=PageView&noscript=1"/></noscript>';
+            echo "<!-- End Facebook Pixel Code -->";
+        });
+    }
