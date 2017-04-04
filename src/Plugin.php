b1b982c6bec2379e63202d002307ea004aee447f
Broholm Kristian <kristianb@admdkristianbr.sk.fi.local>
2017-04-04 13:31:29 +0300

    Fix

--- src/Plugin.php
+++ src/Plugin.php
@@ -13,20 +13,20 @@ namespace kbwp;
 abstract class Plugin {
 
     private static $instances = array();
-    
+
     public static function init($dir, $url) {
-        
+
         $class = get_called_class();
-        
-        if ( !self::$instances[$class] ) {
+
+        if ( !isset(self::$instances[$class]) ) {
             self::$instances[$class] = new $class($dir, $url);
         }
-        
+
         register_activation_hook(__FILE__, array($this, 'activation_hook'));
-        
+
         return self::$instances[$class];
     }
-    
+
     protected function activation_hook() {
         flush_rewrite_rules();
     }
