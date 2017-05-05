ef9ca0e002ed806b0644f37b8546763805428fb7
Broholm Kristian <kristianb@admdkristianbr.local>
2017-05-05 10:54:59 +0300

    Kommentoitu koodia ja lisäilty uusia metodeja

--- src/PostType.php
+++ src/PostType.php
@@ -20,7 +20,15 @@ class PostType {
     public $settings;
     public $labels;
 
-    public function __construct($handle, $name, $is_public = true, $user_settings = array(), $user_labels = array()) {
+    /**
+     * Creates new Post Type to be registered.
+     * @param string        $handle        Slug for post type eg. 'post' or 'page'.
+     * @param string|array  $name          Name of the post type. First value of the array is used as singular_name and second value as plurarl_name.
+     * @param boolean       $is_public     Defines should post type be public or not. Private post types are still visible on the admin side by default.
+     * @param array         $user_settings Custom settings for the post type
+     * @param array         $user_labels   Custom labels for the post type.
+     */
+    public function __construct($handle = '', $name = '', $is_public = true, $user_settings = array(), $user_labels = array()) {
 
         if (is_array($name)) {
             $this->name     = ucfirst($name[0]);
@@ -60,7 +68,7 @@ class PostType {
         $this->settings = array_merge($defaults, $user_settings);
     }
 
-    public function supports($feature = '') {
+    public function add_support($feature = '') {
         if (!is_array($feature)) {
             $features[] = $feature;
         } else {
@@ -68,13 +76,13 @@ class PostType {
         }
 
         foreach($features as $feature) {
-            if (!$this->has_support_for($feature)) {
+            if (!$this->has_support($feature)) {
                 $this->settings['supports'][] = $feature;
             }
         }
     }
 
-    public function has_support_for($feature) {
+    public function has_support($feature) {
         if (is_array($this->settings['supports'])) {
             if (in_array($feature, $this->settings['supports'])) {
                 return true;
@@ -83,7 +91,7 @@ class PostType {
         return false;
     }
 
-    public function does_not_support($feature = '') {
+    public function remove_support($feature = '') {
 
         if (!is_array($feature)) {
             $features[] = $feature;
@@ -92,7 +100,7 @@ class PostType {
         }
 
         foreach($features as $feature) {
-            if ($this->has_support_for($feature)) {
+            if ($this->has_support($feature)) {
                 foreach($this->settings['supports'] as $key => $value) {
                     if ($value == $feature) {
                         unset($this->settings['supports'][$key]);
@@ -102,10 +110,63 @@ class PostType {
         }
     }
 
-    public function get_settings() {
+    public function has_taxonomy($taxonomy) {
+        if (array_key_exists('taxonomies', $this->settings)) {
+            if (in_array($taxonomy, $this->settings['taxonomies'])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function add_taxonomy($taxonomy = '') {
+
+        if (is_array($taxonomy)) {
+            $taxonomies = $taxonomy;
+        } else {
+            $taxonomies[] = $taxonomy;
+        }
+
+        foreach($taxonomies as $taxonomy) {
+            if (!$this->has_taxonomy($taxonomy)) {
+                $this->settings['taxonomies'][] = $taxonomy;
+            }
+        }
+    }
+
+    /**
+     * Removes given taxonomy or taxonomies
+     * @param  string|array     $taxonomy Taxonomies to be removed
+     */
+    public function remove_taxonomy($taxonomy = '') {
+        if (is_array($taxonomy)) {
+            $taxonomies = $taxonomy;
+        } else {
+            $taxonomies[] = $taxonomy;
+        }
+
+        foreach($taxonomies as $taxonomy) {
+            if ($this->has_taxonomy($taxonomy)) {
+                foreach($this->settings['taxonomies'] as $key => $value) {
+                    if ($value == $taxonomy) {
+                        unset($this->settings['taxonomies'][$key]);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Returns Post Type's settings.
+     * @return array $this->settings
+     */
+    private function get_settings() {
         return $this->settings;
     }
 
+    /**
+     * Registers the post type by using WP's register_post_type -function.
+     */
     public function __destruct() {
         register_post_type($this->slug, $this->get_settings());
         unset($this);
