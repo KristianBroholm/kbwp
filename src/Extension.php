71d3bc1ca37cb5a8b1f908755e23fd81956f2ab1
Kristian Broholm <kristian.broholm@kaskiagency.fi>
2020-04-20 16:17:13 +0300

    Lisätty useita uusia metodeja

--- src/Extension.php
+++ src/Extension.php
@@ -6,30 +6,63 @@ namespace kbwp;
 abstract class Extension
 {
 
-  public function addStyle($handle, $src = '', $deps = array(), $version = false, $in_footer = false)
+  private function actionEnqueueScripts($function)
   {
-    wp_enqueue_script($name, $src, $deps, $version, $in_footer);
+    add_action('wp_enqueue_scripts', function() use ($function){
+      $function();
+    });
   }
 
-
-  public function addScript($handle, $src ='', $deps = array(), $version = false, $in_footer = false)
+  public function addStyle($handle,  $src = '',  $deps = array(), $ver = false, $media = 'all')
   {
-    wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
+    $this->actionEnqueueScripts(function() use ($handle, $src, $deps, $ver, $media){
+      wp_enqueue_style($handle,  $src,  $deps, $ver, $media);
+    });
+    return $this;
   }
 
 
-  public function addTypekit( $id = '' ) {
-
-      wp_enqueue_script( 'typekit-' . $id, 'https://use.typekit.net/' . $id . '.js' );
-
-      if ( !has_action( 'wp_head', 'typekit_init' ) ) {
-          add_action( 'wp_head', array( $this, 'typekit_init' ) );
-      }
+  public function addScript($handle,  $src = '',  $deps = array(), $ver = false, $in_footer = false)
+  {
+    $this->actionEnqueueScripts(function() use ($handle, $src, $deps, $ver, $in_footer){
+      wp_enqueue_script($handle,  $src,  $deps, $ver, $in_footer);
+    });
+    return $this;
   }
 
 
-  public function typekit_init() {
+  public function addTypekit( $id = '' )
+  {
+    wp_enqueue_script( 'typekit-' . $id, 'https://use.typekit.net/' . $id . '.js' );
 
-      echo "<script>\n//Init typekit\ntry{Typekit.load({ async: true });}catch(e){}\n</script>";
+    if ( !has_action( 'wp_head', 'typekit_init' ) ) {
+      add_action( 'wp_head', array( $this, 'typekit_init' ) );
+    }
+  }
+
+
+  public function removeStyle($handle)
+  {
+    $this->actionEnqueueScripts(function() use ($handle){
+        wp_dequeue_style($handle);
+        wp_deregister_style($handle);
+    });
+    return $this;
+  }
+
+
+  public function removeScript($handle)
+  {
+    $this->actionEnqueueScripts(function() use ($handle){
+        wp_dequeue_script($handle);
+        wp_deregister_script($handle);
+    });
+    return $this;
+  }
+
+
+  public function typekit_init()
+  {
+    echo "<script>\n//Init typekit\ntry{Typekit.load({ async: true });}catch(e){}\n</script>";
   }
 }
--- src/Theme.php
+++ src/Theme.php
@@ -1,12 +1,43 @@
 <?php
 
 namespace kbwp;
+use Timber\Timber as Timber, \TimberMenu;
 
-abstract class Theme extends Extension
+class Theme extends Extension
 {
+  public $_locale;
+  public $_menus;
+  public $_url;
+  public $_directory;
+  public $_languageFolder;
 
-  public function __construct() {
+  private function actionAfterSetup($function)
+  {
+    add_action('after_setup_theme', function() use ($function) {
+      $function();
+    });
+    return $this;
+  }
 
+
+  private function loadTextDomain ()
+  {
+    $this->actionAfterSetup( function() {
+      load_theme_textdomain( $this->_locale, $this->_directory . $this->_directory . '/' . $this->_languageFolder );
+    });
+  }
+
+
+  public function __construct($locale, $languageFolder = 'lang')
+  {
+    // Define properties
+    $this->_locale          = $locale;
+    $this->_menus           = array();
+    $this->_url             = get_stylesheet_directory_uri();
+    $this->_directory       = get_template_directory();
+    $this->_languageFolder  = $languageFolder;
+
+    // Define supported features
     $this->addSupport('title-tag')
          ->addSupport('custom-logo')
          ->addSupport('post-thumbnails')
@@ -18,45 +49,101 @@ abstract class Theme extends Extension
              'gallery',
              'caption'
          ]);
+
+    $this->loadTextDomain();
   }
 
 
-  private function actionAfterSetup($function)
+  public function addImageSize($name, $width = 0, $height = 0, $crop = false)
   {
-    add_action('after_setup_theme', function() use ($function) {
-      $function();
+    $this->actionAfterSetup(function() use ($name, $width, $height, $crop){
+      add_image_size($name, $width, $height, $crop);
     });
     return $this;
   }
 
 
-  public function addImageSize($name, int $width, int $height, $crop = false)
+  public function addNavigation($location, $name)
   {
-    add_image_size($name, $width, $height, $crop);
-    return $this;
+    $this->actionAfterSetup(function() use ($location, $name) {
+      register_nav_menu($location, $name);
+    });
+    $this->_menus[$location] = $name;
   }
 
 
   public function addNavigations($locations = array())
   {
-      $this->actionAfterSetup(function() use ($locations){
+    $this->actionAfterSetup(function() use ($locations)
+    {
+      register_nav_menus($locations);
+    });
 
-        register_nav_menus($locations);
-      });
-      return $this;
+    foreach($locations as $location => $name)
+    {
+      $this->_menus[$location] = $name;
+    }
+
+    return $this;
+  }
+
+
+  public function addNavigationsToTimberContext()
+  {
+    add_filter('timber_context', array($this, 'filter_add_navigations_to_timber_context'));
   }
 
 
   public function addSupport($feature, $options = array())
   {
-
-    $this->actionAfterSetup(function() use ($feature, $options) {
-
+    $this->actionAfterSetup(function() use ($feature, $options)
+    {
       if ($options) {
         add_theme_support($feature, $options);
       } else {
         add_theme_support($feature);
       }
+    });
+    return $this;
+  }
+
+
+  public function filter_add_navigations_to_timber_context($timber_context)
+  {
+    if (class_exists('\Timber\Menu') && isset($this->_menus))
+    {
+      foreach ($this->_menus as $location => $name)
+      {
+          $timber_context['menu'][$location] = new \Timber\Menu( $name );
+      }
+    }
+    return $timber_context;
+  }
+
+
+  public function removeImageSize($name)
+  {
+    $this->actionAfterSetup(function() use ($name){
+      remove_image_size($name);
+    });
+    return $this;
+  }
+
+
+  public function removeNavigation($location)
+  {
+    $this->actionAfterSetup(function() use ($location){
+      unregister_nav_menu($location);
+    });
+    unset($this->_menus[$location]);
+    return $this;
+  }
+
+
+  public function removeSupport($feature)
+  {
+    $this->actionAfterSetup(function() use ($feature){
+      remove_theme_support($feature);
     });
     return $this;
   }
